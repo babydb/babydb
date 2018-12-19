3c3c8b9ee7d9b536d05ae3956e19e2645207acd8
wangy <yingge_gzb@qq.com>
2018-12-19 18:48:12 +0800

    创建数据库

--- src/b2schema/database.go
+++ src/b2schema/database.go
@@ -1,18 +1,86 @@
 package b2schema
 
 import (
+	"errors"
+	"log"
+	"time"
+
 	rdb "github.com/tecbot/gorocksdb"
 )
 
 // B2Database babydb数据库结构体
 type B2Database struct {
-	Database    string   `json:"Database"`
-	TableList   []string `json:"TableList,omitempty"`
-	DatabaseID  string   `json:"DatabaseID"`
-	RocksDbConn rdb.DB   `json:"-"`
+	// 数据库名称
+	Database string `json:"Database"`
+	// 数据库表列表
+	TableList []string `json:"TableList,omitempty"`
+	// 数据库全局唯一ID，用于作为在rocksdb中的真实数据库ID
+	DatabaseID string `json:"DatabaseID"`
+	// rocksdb连接结构体
+	RocksDbConn *rdb.DB `json:"-"`
+	// 创建时间
+	CreateTime *time.Time `json:"CreateTime,omitempty"`
+	// 打开时间
+	OpenTime *time.Time `json:"OpenTime,omitempty"`
 }
 
-func NewDatabase(name string, tables []string) (*B2Database, error) {
-	// TODO
-	return nil, nil
+// NewDatabase 创建一个新的数据库
+// name 数据库名称
+// meta 元数据库连接结构体指针
+func NewDatabase(name string, meta *MetaDBSource) (*B2Database, error) {
+	// 加锁，保证修改元数据时是原子操作
+	meta.Mu.Lock()
+	defer meta.Mu.Unlock()
+	db, err := meta.GetDatabase(name)
+	if err == nil {
+		log.Fatalf("数据库名称已经存在: %s\n", name)
+		return nil, errors.New("database name duplicated")
+	}
+	db = &B2Database{
+		Database:    name,
+		TableList:   nil,
+		DatabaseID:  "UniqueID", // TODO: Global ID Generator to be called
+		RocksDbConn: nil,
+		CreateTime:  nil,
+		OpenTime:    nil,
+	}
+	if err = meta.PutDatabase(name, db); err != nil {
+		log.Fatalf("创建数据库时发生错误: %v\n", err)
+		return nil, err
+	}
+	return db, nil
+}
+
+// NewDatabaseAndOpen 新建数据库并打开
+func NewDatabaseAndOpen(name string, meta *MetaDBSource) (*B2Database, error) {
+	b2db, err := NewDatabase(name, meta)
+	if err != nil {
+		log.Fatalf("新建数据库时发生错误: %v\n", err)
+		return nil, err
+	}
+	b2db, err = b2db.OpenConnection()
+	if err != nil {
+		log.Fatalf("打开数据库时发生错误: %v\n", err)
+		return nil, err
+	}
+	return b2db, nil
+}
+
+// OpenConnection 打开B2DB数据库连接
+func (b2db *B2Database) OpenConnection() (*B2Database, error) {
+	opts := rdb.NewDefaultOptions()
+	if b2db.CreateTime == nil {
+		opts.SetCreateIfMissing(true)
+		opts.SetErrorIfExists(true)
+	}
+	rocks, err := rdb.OpenDb(opts, b2db.DatabaseID)
+	if err != nil {
+		log.Fatalf("打开数据库连接时发生错误: %v\n", err)
+		return nil, err
+	}
+	nt := time.Now()
+	b2db.RocksDbConn = rocks
+	b2db.CreateTime = &nt
+	b2db.OpenTime = &nt
+	return b2db, nil
 }
--- src/b2schema/metadb.go
+++ src/b2schema/metadb.go
@@ -3,6 +3,8 @@ package b2schema
 import (
 	"encoding/json"
 	"log"
+	"sync"
+	"time"
 
 	rdb "github.com/tecbot/gorocksdb"
 )
@@ -12,21 +14,36 @@ const (
 	METADB = "B2META"
 )
 
+// MetaDBSource 数据库连接结构体
+type MetaDBSource struct {
+	rocksDB  *rdb.DB
+	OpenTime time.Time
+	SyncTime time.Time
+	Mu       *sync.Mutex
+}
+
 // OpenMetaConn 打开元数据库连接，主程序应该保存这个连接
-func OpenMetaConn() *rdb.DB {
+func OpenMetaConn() *MetaDBSource {
 	opts := rdb.NewDefaultOptions()
 	opts.SetCreateIfMissing(true)
 	meta, err := rdb.OpenDb(opts, METADB)
 	if err != nil {
 		log.Panicf("无法打开Meta元数据库连接，此服务器已经存在严重错误，服务即将退出: %v\n", err)
 	}
-	return meta
+	nt := time.Now()
+	mutex := &sync.Mutex{}
+	return &MetaDBSource{
+		rocksDB:  meta,
+		OpenTime: nt,
+		SyncTime: nt,
+		Mu:       mutex,
+	}
 }
 
 // GetDatabase 在元数据中查找某个名称的数据库
-func GetDatabase(dbname string, meta *rdb.DB) (*B2Database, error) {
+func (c *MetaDBSource) GetDatabase(dbname string) (*B2Database, error) {
 	opts := rdb.NewDefaultReadOptions()
-	slice, err := meta.Get(opts, []byte(dbname))
+	slice, err := c.rocksDB.Get(opts, []byte(dbname))
 	if err != nil {
 		log.Printf("找不到名称为 %s 的数据库: %v\n", dbname, err)
 		return nil, err
@@ -37,4 +54,20 @@ func GetDatabase(dbname string, meta *rdb.DB) (*B2Database, error) {
 		return nil, err
 	}
 	return db, nil
+}
+
+// PutDatabase 将某个数据库PUT更新到元数据中
+func (c *MetaDBSource) PutDatabase(dbname string, dbstruct *B2Database) error {
+	opts := rdb.NewDefaultWriteOptions()
+	opts.SetSync(true)
+	dbContent, err := json.Marshal(dbstruct)
+	if err != nil {
+		log.Fatalf("数据库META转换为JSON时发生错误: %v\n", err)
+		return err
+	}
+	if err = c.rocksDB.Put(opts, []byte(dbname), dbContent); err != nil {
+		log.Fatalf("将数据库META写入rocksdb时发生错误: %v\n", err)
+		return err
+	}
+	return nil
 }
