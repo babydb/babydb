5bb49d27866e3ce8486a98d6693d4adab712bb5f
i_yingwang(王滢) <i_yingwang@tencent.com>
2019-05-28 15:04:27 +0800

    indexing update

--- core/indexing.go
+++ core/indexing.go
@@ -3,6 +3,8 @@ package core
 import (
 	"bytes"
 	"errors"
+	"log"
+	"encoding/binary"
 
 	"github.com/google/btree"
 	schema "github.com/babydb/babydb/b2schema"
@@ -132,35 +134,69 @@ func (a NormalIndex) Serialize(indexID string) ([]byte, error) {
 		return nil, errors.New("index ID not found")
 	}
 	var buf bytes.Buffer
-	tree.Ascend(idTraverse(&buf))
+	tree.Ascend(normalTraverse(&buf))
 	treeBytes := make([]byte, buf.Len())
 	buf.Read(treeBytes)
 	return treeBytes, nil
 }
 
-func Deserialize(treeBytes []byte) (*btree.BTree, error) {
-	if len(treeBytes) == 0 {
+// IdIndexDeserialize 将一个byte数组反序列化未一个ID索引
+func IdIndexDeserialize(treeBytes []byte) (*btree.BTree, error) {
+	bsLen := len(treeBytes)
+	if bsLen == 0 {
 		return nil, errors.New("empty bytes to deserialize")
 	}
 	tree := btree.New(64)
-	p := 0
-	for i := 0; i < len(treeBytes); {
-		if treeBytes[i] == 0 && treeBytes[i+1] == 0 {
-			var id IDIndex = treeBytes[p:i]
-			tree.ReplaceOrInsert(id)
-			i += 2
-			p = i
+	for p:=0; p<bsLen; {
+		hl, size := binary.Varint(treeBytes[p:])
+		if size == 0 {
+			log.Fatalf("读取Varint时发生错误，字节缓冲区长度不足\n")
+			break
+		}
+		if size < 0 {
+			log.Printf("读取到的Varint长度值超过64bit，该长度忽略")
+			p += size
 			continue
 		}
-		i++
+		p += size
+		if p+hl>bsLen {
+			log.Fatalf("ID数据长度范围超出字节总长度范围: %d > %d\n", p+hl, bsLen)
+			break
+		}
+		id := treeBytes[p: p+hl].(IDIndex)
+		tree.ReplaceOrInsert(id)
+		p += hl
 	}
 	return tree, nil
 }
 
+func NormalIndexDeserialize(treeBytes []byte) (*btree.BTree, error) {
+	bsLen := len(treeBytes)
+	if bsLen == 0 {
+		return nil, errors.New("empty bytes to deserialize")
+	}
+	tree := btree.New(64)
+	for p:=0; p<bsLen; {
+		hl, size := binary.Varint(treeBytes[p:])
+		if size == 0 {
+			log.Fatalf("读取Varint时发生错误，字节缓冲区长度不足\n")
+			break
+		}
+		if size < 0 {
+			log.Printf("读取到的Varint长度值超过64bit，该长度忽略")
+			p += size
+			continue
+		}
+		
+	}
+}
+
 func idTraverse(buf *bytes.Buffer) btree.ItemIterator {
 	return func(i btree.Item) bool {
+		lenBuf := make([]byte, 8)
+		hl := binary.PutVarint(lenBuf, len(i.(IDIndex)))
+		buf.Write(lenBuf[:hl])
 		buf.Write(i.(IDIndex))
-		buf.Write([]byte{0, 0})
 		return true
 	}
 }
@@ -172,7 +208,44 @@ func normalTraverse(buf *bytes.Buffer) btree.ItemIterator {
 			log.Fatalf("节点不是普通索引，btree类型错误。")
 			return false
 		}
-		schema.
-		buf.Write(data.Value)
+		lenBuf := make([]byte, 8)
+		var hl int
+		switch data.Value.(type) {
+		case int32:
+			hl = binary.PutVarint(lenBuf, (int64) 4)
+			buf.Write(lenBuf[:hl])
+			buf.Write(schema.Int32ToBytes(data.Value.(int32)))
+		case int64:
+			hl = binary.PutVarint(lenBuf, (int64) 8)
+			buf.Write(lenBuf[:hl])
+			buf.Write(schema.Int64ToBytes(data.Value.(int64)))
+		case float32:
+			hl = binary.PutVarint(lenBuf, (int64) 4)
+			buf.Write(lenBuf[:hl])
+			buf.Write(schema.Float32ToBytes(data.Value.(float32)))
+		case float64:
+			hl = binary.PutVarint(lenBuf, (int64) 8)
+			buf.Write(lenBuf[:hl])
+			buf.Write(schema.Float64ToBytes(data.Value.(float64)))
+		case string:
+			hl = binary.PutVarint(lenBuf, (int64) len(([]byte) data.Value.(string)))
+			buf.Write(lenBuf[:hl])
+			buf.Write(([]byte)data.Value.(string))
+		case []byte:
+			hl = binary.PutVarint(lenBuf, (int64) len(data.Value.([]byte)))
+			buf.Write(lenBuf[:hl])
+			buf.Write(data.Value.([]byte))
+		default:
+			log.Printf("节点数据类型不可识别：%t\n", data.Value)
+			return false
+		}
+		hl = binary.PutVarint(lenBuf, (int64) len(data.UID))
+		buf.Write(lenBuf[:hl])
+		for _, id := range data.UID {
+			hl = binary.PutVarint(lenBuf, (int64) len(([]byte)id))
+			buf.Write(lenBuf[:hl])
+			buf.Write(([]byte)id)
+		}
 	}
 }
+
